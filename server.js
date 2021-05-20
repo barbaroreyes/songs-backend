62bd6a34a5c44792c9748ce869182a5fee7cf80a
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2021-05-20 08:48:07 -0500

    added-server

---
+++ server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+// const mongoose = require("./db/connection");
+// const sevenwRouter = require("./controllers/sevenwonder")
+const app = express();
+const PORT = process.env.PORT
+//imports
+const cors = require("cors")
+const morgan = require("morgan")
+
+//midleware
+app.use(cors())
+app.use(express.json())
+app.use(morgan('dev'))
+
+
+app.get("/", (req, res) => {
+    res.json({ hello: "Hello World!" });
+  });
+//   app.use('/sevenw',sevenwRouter)
+
+app.listen(PORT, () => {
+  console.log(`Your are listening on port ${PORT}`);
+});
